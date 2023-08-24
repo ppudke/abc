93e8da2305537abb97eca407914484936cd8d5f9
ppudke <ppudke@PPUDKE2L2.ptcnet.ptc.com>
2023-08-24 18:07:43 +0530

    cjc practice

---
+++ August24/src/august24session/B.java
@@ -0,0 +1,11 @@
+package august24session;
+
+public class B extends A{
+	
+	public void m2()
+	{
+		System.out.println("m2---B");
+	}
+
+
+}
---
+++ August24/src/august24session/Test.java
@@ -0,0 +1,15 @@
+package august24session;
+
+public class Test {
+	public static void main(String[] args) {
+		
+	
+	B b = new B();
+	
+	b.m1();
+	b.m2();
+	A a = new B();
+	a.m1();
+	}
+
+}
